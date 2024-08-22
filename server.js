f596c72e0cdf9e08829981b63ad4ff5102435a62
vsriramakarthik <147160205+vsriramakarthik@users.noreply.github.com>
2024-08-22 20:13:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const userRoutes = require("./routes/userRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+require("dotenv").config();
+require("./db");
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use("/users", userRoutes);
+app.use("/tasks", taskRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Task Manager API is working!",
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
